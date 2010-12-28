f4bb9a8acfe034544bf675026fd86b9e6c8c7849
Henrik Kirk <henrik@busywait.org>
2010-12-28 14:10:43 +0100

    Moved constants into file

---
+++ F1Constants.php
@@ -0,0 +1,25 @@
+<?php
+
+// 2010 races
+$races = array(0 => "Bahrain", 1 => "Melbourne - Australia",
+    2 => "Sepang - Malaysia", 3 => "Shanghai - China", 4 => "Barcelona - Spain",
+    5 => "Monte Carlo - Monaco", 6 => "Istanbul - Turkey",
+    7 => "Montreal - Canada", 8 => "Valencia - Europe",
+    9 => "Silverstone - Britain", 10 => "Hockenheim - Germany",
+    11 => "Budapest - Hungary", 12 => "Spa - Belgium", 13 => "Monza - Italy",
+    14 => "Singapore", 15 => "Suzuka - Japan", 16 => "Yeongam - Korea",
+    17 => "Interlagos - Brazil", 18 => "Abu Dhabi");
+
+// Race types
+$type = array(0 => "Practice", 1 => "Qualifying", 2 => "Race");
+
+// Tyre types
+$tyre = array(0 => "Option", 1 => "Prime", 2 => "Intermediate", 3 => "Wet");
+
+// Cars
+$cars = array(0 => "RBR-Renault", 1 => "McLaren-Macedes", 2 => "Farrari",
+    3 => "Macedes-GP", 4 => "Renault", 5 => "Williams-Cosworth",
+    6 => "Force India-Macedes", 7 => "BMW Sauber-Farrari", 8 => "STR-Farrai",
+    9 => "Lotus-Cosworth", 10 => "HRT-Cosworth", 11 => "Virgen-Cosworth");
+
+?>
